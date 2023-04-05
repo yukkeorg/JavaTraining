06940bbfe905c4dda7a97c3559cccc2e75a56f45
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2023-04-05 23:05:33 +0900

    リフォーマット

--- 03_webapp/src/main/java/com/s_giken/traning/webapp/WebappApplication.java
+++ 03_webapp/src/main/java/com/s_giken/traning/webapp/WebappApplication.java
@@ -3,7 +3,6 @@ package com.s_giken.traning.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class WebappApplication {
 	public static void main(String[] args) {
