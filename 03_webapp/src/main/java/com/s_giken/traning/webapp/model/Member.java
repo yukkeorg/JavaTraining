712d3ad9b666081420fbbd5b38db33ba50764a05
Ohshima Yusuke <ooshima@s-giken.com>
2023-04-07 17:13:18 +0900

    足りないアノテーション等を追加

--- 03_webapp/src/main/java/com/s_giken/traning/webapp/model/Member.java
+++ 03_webapp/src/main/java/com/s_giken/traning/webapp/model/Member.java
@@ -1,12 +1,21 @@
 package com.s_giken.traning.webapp.model;
 
 
+import java.sql.Timestamp;
 import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,24 +27,40 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor // 全てのメンバ変数に対する引数を持つコンストラクタを自動生成
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "member_id")
     private int memberId;
 
     @Column(name = "mail")
+    @NotNull
+    @NotBlank
     private String mail;
 
     @Column(name = "name")
+    @NotNull
+    @NotBlank
     private String name;
 
     @Column(name = "address")
+    @NotNull
     private String address;
 
     @Column(name = "start_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull
     private Date startDate;
 
     @Column(name = "end_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date endDate;
 
     @Column(name = "payment_method")
+    @NotNull
     private int paymentMethod;
+
+    @CreatedDate
+    private Timestamp createdDate;
+
+    @LastModifiedDate
+    private Timestamp updatedDate;
 }
