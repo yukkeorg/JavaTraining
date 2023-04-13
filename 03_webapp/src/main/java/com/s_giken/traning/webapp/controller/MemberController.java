9c4a6105d2fe3c6f9d24191234b3c31c62a5c9e7
Ohshima Yusuke <ooshima@s-giken.com>
2023-04-13 18:00:34 +0900

    リファクタリング

--- 03_webapp/src/main/java/com/s_giken/traning/webapp/controller/MemberController.java
+++ 03_webapp/src/main/java/com/s_giken/traning/webapp/controller/MemberController.java
@@ -10,7 +10,6 @@ import com.s_giken.traning.webapp.exception.NotFoundException;
 import com.s_giken.traning.webapp.model.Member;
 import com.s_giken.traning.webapp.model.MemberSearchCondition;
 import com.s_giken.traning.webapp.service.MemberService;
-import java.lang.ProcessBuilder.Redirect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
