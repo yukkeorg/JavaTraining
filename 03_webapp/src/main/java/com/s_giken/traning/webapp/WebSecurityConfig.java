c6dcd2b774fc762a1e2c8e436c55755285559780
Ohshima Yusuke <ooshima@s-giken.com>
2023-04-19 18:00:33 +0900

    Spring Security の設定見直し

--- 03_webapp/src/main/java/com/s_giken/traning/webapp/WebSecurityConfig.java
+++ 03_webapp/src/main/java/com/s_giken/traning/webapp/WebSecurityConfig.java
@@ -2,28 +2,38 @@ package com.s_giken.traning.webapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
     // Spring Securityの設定
     // ルート以下のアクセスは認証が必要
     // ログインページはカスタムの /login
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/**").authenticated()
-                .anyRequest().denyAll())
-                .formLogin((formLogin) -> formLogin
+    public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // CSRF対策を無効化
+                .formLogin((form) -> form
+                        .defaultSuccessUrl("/")
+                        .loginProcessingUrl("/login")
                         .loginPage("/login")
-                        .failureForwardUrl("/login-error")
-                        .permitAll());
+                        .failureUrl("/login?error")
+                        .permitAll())
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/"))
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated());
+
         return http.build();
     }
 
@@ -35,9 +45,15 @@ public class WebSecurityConfig {
         var user = User
                 .builder()
                 .username("user")
-                .password("{noop}password")
+                .password("$2a$10$Jjf89PMWEuE1JOFF57rw2.Ny64pJbWTnvJB5PBLSbP27R6.nKOC1e") // password
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    // ログインフォーム等から送られてくるパスワードをハッシュ化する
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
