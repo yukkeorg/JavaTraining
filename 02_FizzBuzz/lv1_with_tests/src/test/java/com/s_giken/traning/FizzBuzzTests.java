86adb96e86675b77c1d7faa651226ed2640ed08e
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2023-04-30 14:59:34 +0900

    テスト項目に誤りがあったので修正

--- 02_FizzBuzz/lv1_with_tests/src/test/java/com/s_giken/traning/FizzBuzzTests.java
+++ 02_FizzBuzz/lv1_with_tests/src/test/java/com/s_giken/traning/FizzBuzzTests.java
@@ -9,14 +9,17 @@ public class FizzBuzzTests {
     @Test
     public void correct_FizzBuzz() {
         assertEquals("1", app.fizzBuzz(1));
-        assertEquals("10", app.fizzBuzz(10));
-
+        assertEquals("2", app.fizzBuzz(2));
         assertEquals("Fizz", app.fizzBuzz(3));
+        assertEquals("4", app.fizzBuzz(4));
         assertEquals("Buzz", app.fizzBuzz(5));
-        assertEquals("FizzBuzz", app.fizzBuzz(15));
-
         assertEquals("Fizz", app.fizzBuzz(6));
+        assertEquals("7", app.fizzBuzz(7));
+        assertEquals("8", app.fizzBuzz(8));
+        assertEquals("Fizz", app.fizzBuzz(9));
         assertEquals("Buzz", app.fizzBuzz(10));
+
+        assertEquals("FizzBuzz", app.fizzBuzz(15));
         assertEquals("FizzBuzz", app.fizzBuzz(30));
     }
 }
