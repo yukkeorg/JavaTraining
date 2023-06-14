58585d44ea1f42858fa2799954e779b05ac04b43
Ohshima Yusuke <ooshima@s-giken.com>
2023-06-14 10:10:56 +0900

    Remove folders except 03_webapp containts.

--- 02_FizzBuzz/lv2/src/com/s_giken/traning/IFizzBuzz.java
+++
@@ -1,5 +0,0 @@
-package com.s_giken.traning;
-
-public interface IFizzBuzz {
-    public String translate(int num);
-}
--- 02_FizzBuzz/lv4/README.md
+++
@@ -1,5 +0,0 @@
-# FizzBuzz問題 問4回答例
-
-## 実行方法
-
-VSCodeでこのファイルを開き、src/com/s_giken/training/App.java の main 関数を実行します。
