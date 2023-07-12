591a38a127c04702d11b30aa34a299127921ac9e
Ohshima Yusuke <ooshima@s-giken.com>
2023-07-12 11:13:16 +0900

    リポジトリアクセスインターフェース定義時の特殊ルールについてコメント

--- src/main/java/com/s_giken/training/webapp/repository/MemberRepository.java
+++ src/main/java/com/s_giken/training/webapp/repository/MemberRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.s_giken.training.webapp.model.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-    // 参考
+    // SprintBootでは、リポジトリアクセス用のインターフェースを定義する際に
+    // 特殊な命名ルールがあります。詳細は以下の参考先URLを確認して下さい。
     // https://docs.spring.io/spring-data/jpa/docs/1.11.1.RELEASE/reference/html/#jpa.query-methods
     public List<Member> findByNameLikeOrMailLike(String name, String mail);
 }
